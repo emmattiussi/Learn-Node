2123d5c698893b6a3b39436ca68c107df2308960
Emily <emily.mattiussi@gmail.com>
2020-06-15 08:29:52 +0100

    Work for video 18, plus gitignoring uploaded images.

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -6,10 +6,21 @@ const { catchErrors } = require('../handlers/errorHandlers.js')
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
-router.get('/stores', catchErrors(storeController.getStores));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores', catchErrors(storeController.getStores));
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 
 module.exports = router;
